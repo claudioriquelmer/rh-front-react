1ebf2b7e71d0376c98559e4d3121b88b605ffebe
ClaudioRiquelmer <claudio.riquelmer@outlook.es>
2024-08-05 13:36:47 -0400

    Front Agregar Empleado

--- recursos-humanos-app/src/App.js
+++ recursos-humanos-app/src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import AgregarEmpleado from "./empleados/AgregarEmpleado";
 import Navegacion from "./plantilla/Navegacion";
 
 function App() {
   return (
     <div className='container'>
-      <Navegacion />
-      <ListadoEmpleados />
+      <BrowserRouter>
+        <Navegacion/>
+          <Routes>
+            <Route exact path="/" element={<ListadoEmpleados/>}/>
+            <Route exact path="/agregar" element={<AgregarEmpleado/>}/>
+          </Routes>
+      </BrowserRouter>
     </div>
 
   );
